40c89de424b1fdfaecf444274524147fb4f521be
MasthomT <masthom.pro@gmail.com>
2026-02-04 16:47:10 +0100

    refonte totale

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const CONFIG = {
-    SERVER_URL: "https://prime-nearby-tick.ngrok-free.app", 
+    // TON NOUVEL ADRESSE NGROK (Vu dans ta session active)
+    SERVER_URL: "https://f5024db2450c.ngrok-free.app", 
+    
+    // TON IP LOCALE RASPBERRY
     RASPBERRY_IP: "http://192.168.1.109:8080",            
     
+    // INFOS TWITCH
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
     BROADCASTER_ID: "439356462",
     BROADCASTER_NAME: "masthom_",
-
 };
