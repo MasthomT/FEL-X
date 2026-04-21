f7c9233a71b58b199eed0408f264c2b406631b4f
MasthomT <masthom.pro@gmail.com>
2026-04-21 14:22:12 +0200

    transfert FASTAPI

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: "https://carl-bytes-depot-salon.trycloudflare.com/api", 
+    API_BASE_URL: "https://wendy-explorer-reunion-carlos.trycloudflare.com/api", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
     BROADCASTER_NAME: "masthom_",
     BROADCASTER_ID: "439356462"
