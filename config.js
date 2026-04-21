d8091d6f967cf649a2ececf09a3137548d3ae68f
MasthomT <masthom.pro@gmail.com>
2026-04-21 10:25:09 +0200

    transfert FASTAPI

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
-    // TON ADRESSE NGROK ACTUELLE (La bonne !)
-    SERVER_URL: "https://prime-nearby-tick.ngrok-free.app", 
-    
-    RASPBERRY_IP: "http://192.168.1.109:8080",            
+    API_BASE_URL: "prime-nearby-tick.ngrok-free.app/api", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
+    BROADCASTER_NAME: "masthom_"
     BROADCASTER_ID: "439356462",
-    BROADCASTER_NAME: "masthom_",
 };
