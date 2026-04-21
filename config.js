20a1ad4e667513bb0725bbab1ca473cad1658da2
MasthomT <masthom.pro@gmail.com>
2026-04-21 20:38:47 +0200

    transfert FASTAPI

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 const CONFIG = {
-    // ⚠️ Remplace par ton vrai lien Cloudflare (avec /api/v1 à la fin)
-    API_BASE_URL: "prime-nearby-tick.ngrok-free.app/api/v1", 
-    
-    // Ton Client ID Twitch
+    API_BASE_URL: "https://prime-nearby-tick.ngrok-free.app/api/v1", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
-    
-    // Le nom de ta chaîne
     BROADCASTER_NAME: "masthom_",
-
-    // L'adresse exacte de ton site Vercel
     REDIRECT_URI: "https://fel-x.vercel.app"
 };
