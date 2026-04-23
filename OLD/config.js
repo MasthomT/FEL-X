335157ee93c7fe3271f2fb0e84af8fa1e5044ea0
MasthomT <masthom.pro@gmail.com>
2026-04-23 11:17:31 +0200

    transfert FASTAPI

--- OLD/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    API_BASE_URL: "https://wendy-explorer-reunion-carlos.trycloudflare.com/api", 
-    CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
-    BROADCASTER_NAME: "masthom_",
-    BROADCASTER_ID: "439356462"
-};
