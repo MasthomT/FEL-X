f32a5bd53794bd80709b580b55ae5a682dc557c9
MasthomT <masthom.pro@gmail.com>
2026-05-13 12:20:24 +0200

    Modifications Commandes

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    API_BASE_URL: "https://prime-nearby-tick.ngrok-free.app/api/v1", 
+
+    API_BASE_URL: "/api/bot/v1", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
     BROADCASTER_NAME: "masthom_",
     REDIRECT_URI: "https://fel-x.vercel.app"
