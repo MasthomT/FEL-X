6167abc063d11285a204496158e39d374f0d0518
MasthomT <masthom.pro@gmail.com>
2026-05-25 01:06:04 +0200

    Bye NGrok

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: "https://circuits-utilize-vista-graphs.trycloudflare.com", 
+    API_BASE_URL: "https://b488-2a0d-3341-cdc3-310-2e58-e854-9dd9-8bf7.ngrok-free.app", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
     BROADCASTER_NAME: "masthom_",
     REDIRECT_URI: "https://fel-x.netlify.app"
