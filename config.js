4bc12a2b4d25c2c6682c245cea2a14b77439739a
MasthomT <masthom.pro@gmail.com>
2026-05-25 00:50:17 +0200

    Bye NGrok

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: "https://manufacturing-exclude-crew-enters.trycloudflare.com/api/v1", 
+    API_BASE_URL: "https://certificate-crm-thompson-beads.trycloudflare.com", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
     BROADCASTER_NAME: "masthom_",
     REDIRECT_URI: "https://fel-x.netlify.app"
