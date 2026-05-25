4d4f9ab0639990a6ef4e76d59be2a407937bb9d2
MasthomT <masthom.pro@gmail.com>
2026-05-25 11:33:19 +0200

    Bye NGrok

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: "https://b488-2a0d-3341-cdc3-310-2e58-e854-9dd9-8bf7.ngrok-free.app", 
+    API_BASE_URL: "https://v48q8oi53otc.share.zrok.io", 
     CLIENT_ID: "kgyfzs0k3wk8enx7p3pd6299ro4izv",
     BROADCASTER_NAME: "masthom_",
     REDIRECT_URI: "https://fel-x.netlify.app"
